5f0d00373a33f830c210a42ce015b7409987c549
Max Kaplan <max@kraken.com>
2018-01-17 16:24:05 -0500

    uncommenting tests

--- __tests__/authorizer-test.js
+++ __tests__/authorizer-test.js
@@ -28,24 +28,24 @@ describe('authorizer.js', () => {
         sharedFetchSecret,
     }));
 
-    // it('should successfully extract token (extractToken())', () => {
-    //     const mockToken = faker.random.word();
-    //     const token = Authorizer.extractToken({ headers: { authorization: `Bearer ${mockToken}`}});
-    //     expect(token === mockToken).toBe(true);
-    // });
+    it('should successfully extract token (extractToken())', () => {
+        const mockToken = faker.random.word();
+        const token = Authorizer.extractToken({ headers: { authorization: `Bearer ${mockToken}`}});
+        expect(token === mockToken).toBe(true);
+    });
 
-    // it('should fetch package.json', async () => {
-    //     const cb = jest.fn();
-    //     const mockToken = faker.random.word();
-    //     const test = await Authorizer.loadPackageJson({ path }, frontDoorHost, sharedFetchSecret, cb);
-    //     expect(cb).toBeCalledWith(null, packageJson);
-    // });
+    it('should fetch package.json', async () => {
+        const cb = jest.fn();
+        const mockToken = faker.random.word();
+        const test = await Authorizer.loadPackageJson({ path }, frontDoorHost, sharedFetchSecret, cb);
+        expect(cb).toBeCalledWith(null, packageJson);
+    });
 
-    // it('should parse repo url', () => {
-    //     const url = Authorizer.parseRepoUrl(packageJson);
-    //     const gitLabPath = path.split('/');
-    //     expect(url).toEqual({ org: gitLabPath[1], repo: gitLabPath[2] });
-    // });
+    it('should parse repo url', () => {
+        const url = Authorizer.parseRepoUrl(packageJson);
+        const gitLabPath = path.split('/');
+        expect(url).toEqual({ org: gitLabPath[1], repo: gitLabPath[2] });
+    });
 
     it('should authorize correctly', async () => {
         const token = encodeURIComponent(faker.random.word());
